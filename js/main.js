5507697297c18c77e08b073c9a4ce194ea023afd
Farhad Aliyev <farhadaliyev@Farhads-MacBook-Air.local>
2026-04-27 22:39:40 +0400

    second demo try

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Mobile navigation toggle
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+// Close menu when a nav link is clicked
+navLinks.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('open');
+  });
+});
